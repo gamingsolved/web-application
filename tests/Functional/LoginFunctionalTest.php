011d98b5e96c70ecd7a3dfa7922aec6e79f0cd93
Manuel Kiessling <manuel@kiessling.net>
2017-03-19 00:19:31 +0100

    Specced login.

---
+++ tests/Functional/LoginFunctionalTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\Helpers\Helpers;
+
+class LoginFunctionalTest extends WebTestCase
+{
+    use Helpers;
+
+
+    public function testLoginWorksAndLeadsToRemoteDesktopsList()
+    {
+        $this->resetDatabase();
+
+        $client = $this->getClientThatRegisteredAndActivatedAUser();
+        $client->followRedirects();
+
+        $client->request('GET', '/logout');
+        $crawler = $client->request('GET', '/login');
+
+        $buttonNode = $crawler->selectButton('Log in');
+
+        $form = $buttonNode->form();
+
+        $crawler = $client->submit($form, array(
+            '_username' => 'testuser@example.com',
+            '_password' => 'test123'
+        ));
+
+        $this->assertEquals('Your remote desktops', $crawler->filter('h1')->text());
+    }
+
+}
