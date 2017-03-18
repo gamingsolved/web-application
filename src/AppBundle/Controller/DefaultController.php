90f749a73f8102ae3c56419c1a4d3e06f776ab4e
Manuel Kiessling <manuel@kiessling.net>
2017-03-18 17:47:44 +0100

    Setting up multiple languages.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * The special root "/" (without a langauge tag) ends up here.
+     *
+     * Because we want users to always be on a language path, we redirect
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function rootAction()
+    {
+        return $this->redirect('/en/');
+    }
+
     public function indexAction(Request $request)
     {
         return $this->render('default/index.html.twig', [
