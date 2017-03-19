c1665be7147cb52dbc3905c0efa04ff9a3e23e1c
Manuel Kiessling <manuel@kiessling.net>
2017-03-19 10:44:48 +0100

    More first time remote desktop launch specs.

--- tests/Functional/CreateRemoteDesktopsFunctionalTest.php
+++ tests/Functional/CreateRemoteDesktopsFunctionalTest.php
@@ -44,15 +44,41 @@ class CreateRemoteDesktopsFunctionalTest extends WebTestCase
 
         $this->assertNotContains('You do not yet have any remote desktops.', $crawler->filter('body')->text());
 
-        $this->assertContains('My first remote desktop', $crawler->filter('h2')->text());
+        $this->assertContains('My first remote desktop', $crawler->filter('h2')->first()->text());
 
-        $this->assertContains('For playing computer games', $crawler->filter('span.label-info')->text());
+        $this->assertContains('For playing computer games', $crawler->filter('span.label-info')->first()->text());
 
         // Two checks due to line break
-        $this->assertContains('Current status:', $crawler->filter('span.label-default')->text());
-        $this->assertContains('not running', $crawler->filter('span.label-default')->text());
+        $this->assertContains('Current status:', $crawler->filter('span.label-default')->first()->text());
+        $this->assertContains('not running', $crawler->filter('span.label-default')->first()->text());
 
-        $this->assertContains('Launch this remote desktop', $crawler->filter('a.btn')->text());
+        $this->assertContains('Launch this remote desktop', $crawler->filter('a.btn')->first()->text());
+
+        // We want to build on this in another test
+        return $client;
+    }
+
+    public function testLaunchRemoteDesktop()
+    {
+        $client = $this->testCreatingRemoteDesktop();
+
+        $crawler = $client->request('GET', '/en/remoteDesktops/');
+
+        $link = $crawler->selectLink('Launch this remote desktop')->first()->link();
+
+        $crawler = $client->click($link);
+
+        // Verify that we wen into the new instance creation workflow
+        $container = $client->getContainer();
+        $em = $container->get('doctrine.orm.entity_manager');
+        $repo = $em->getRepository('AppBundle\Entity\RemoteDesktop\RemoteDesktop');
+        $remoteDesktop = $repo->findOneBy(['title' => 'My first remote desktop']);
+        $this->assertEquals(
+            '/en/remoteDesktops/' . $remoteDesktop->getId() . '/cloudInstances/new',
+            $client->getRequest()->getRequestUri()
+        );
+
+        $this->assertContains('Launch your remote desktop', $crawler->filter('h1')->first()->text());
     }
 
 }
