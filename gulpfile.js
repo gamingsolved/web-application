4059264927e1343affe89e9dce3063eb101ba167
Primajin <github@eyesbound.com>
2017-04-02 18:18:57 +0200

    add bootstrap and bootstrap-material

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,41 @@
 
 var gulp = require('gulp');
 
+//COPY STUFF
+gulp.task('copy-jquery', function() {
+  return gulp.src('./node_modules/jquery/dist/jquery.min.*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/js'));
+});
+
+gulp.task('copy-bootstrap', function() {
+  return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/css'));
+});
+
+gulp.task('copy-bootstrap-js', function() {
+  return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/js'));
+});
+
+gulp.task('copy-bootstrap-fonts', function() {
+  return gulp.src('./node_modules/bootstrap/dist/fonts/**/*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/fonts'));
+});
+
+gulp.task('copy-bootstrap-material', function() {
+  return gulp.src('./node_modules/bootstrap-material-design/dist/css/*.min.*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/css'));
+});
+
+gulp.task('copy-bootstrap-material-js', function() {
+  return gulp.src('./node_modules/bootstrap-material-design/dist/js/*.min.*')
+    .pipe(gulp.dest('./src/AppBundle/Resources/public/vendor/js'));
+});
+
 //SASS LINTING
 var sassLint = require('gulp-sass-lint');
 gulp.task('sass-lint', function () {
-  return gulp.src('sass/**/*.s+(a|c)ss')
+  return gulp.src(['./sass/**/*.s+(a|c)ss', '!./sass/bootstrap/**/*', '!./sass/bootstrap-material/**/*'])
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
@@ -20,7 +51,7 @@ var insert = require('gulp-insert');
 var pjson = require('./package.json');
 var sass = require('gulp-sass');
 gulp.task('sass', ['sass-lint'], function () {
-  return gulp.src('sass/**/*.s+(a|c)ss')
+  return gulp.src('./sass/**/*.s+(a|c)ss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gcmq())
     .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
@@ -35,12 +66,12 @@ gulp.task('sass', ['sass-lint'], function () {
 
 //WATCHER
 gulp.task('watch', ['sass'], function (cb) {
-  gulp.watch(['sass/**/*.s+(a|c)ss'], ['sass']);
+  gulp.watch(['./sass/**/*.s+(a|c)ss'], ['sass']);
   cb();
 });
 
 //RUN
 var runSequence = require('run-sequence');
 gulp.task('default', function (cb) {
-  runSequence(['watch'], cb);
+  runSequence(['copy-jquery', 'copy-bootstrap', 'copy-bootstrap-js', 'copy-bootstrap-material', 'copy-bootstrap-material-js', 'copy-bootstrap-fonts'], ['watch'], cb);
 });
