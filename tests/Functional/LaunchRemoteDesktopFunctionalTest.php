780345687e076d06ca3bb544bebe248695013b98
Manuel Kiessling <manuel@kiessling.net>
2017-04-27 11:16:04 +0200

    Communicating provisioning costs.
    See https://trello.com/c/yyymy2xA/92-ebs-volumes-need-to-be-billed.

--- tests/Functional/LaunchRemoteDesktopFunctionalTest.php
+++ tests/Functional/LaunchRemoteDesktopFunctionalTest.php
@@ -27,8 +27,11 @@ class LaunchRemoteDesktopFunctionalTest extends WebTestCase
 
         $this->assertContains('My first remote desktop', $crawler->filter('h2')->first()->text());
 
-        $this->assertContains('Costs per hour', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+        $this->assertContains('Usage costs per hour', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
         $this->assertContains('(only in status Ready to use): $1.49', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+
+        $this->assertContains('Current storage costs per hour', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+        $this->assertContains('(until desktop is removed): $0.04', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
 
         $this->assertContains('Current status:', $crawler->filter('h3')->first()->text());
 
@@ -132,8 +135,11 @@ class LaunchRemoteDesktopFunctionalTest extends WebTestCase
 
         $this->assertContains('My first remote desktop', $crawler->filter('h2')->first()->text());
 
-        $this->assertContains('Current hourly costs', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+        $this->assertContains('Current usage costs per hour', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
         $this->assertContains('(while in status Ready to use): $1.49', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+
+        $this->assertContains('Current storage costs per hour', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
+        $this->assertContains('(until desktop is removed): $0.04', $crawler->filter('div.hourlyusagecostsbox')->first()->text());
 
         $this->assertContains('Current status:', $crawler->filter('h3')->first()->text());
         $this->assertContains('Ready to use', $crawler->filter('.remotedesktopstatus')->first()->text());
