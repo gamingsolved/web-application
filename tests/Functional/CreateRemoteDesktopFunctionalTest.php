7abb88e7328074072dd02d226a721b858bb65d81
Manuel Kiessling <manuel@kiessling.net>
2017-04-28 21:50:38 +0200

    Wording, trademarks.
    Solves https://trello.com/c/J5jnfLiv/43-mention-all-the-other-trademarks-we-are-using.

--- tests/Functional/CreateRemoteDesktopFunctionalTest.php
+++ tests/Functional/CreateRemoteDesktopFunctionalTest.php
@@ -67,7 +67,7 @@ class CreateRemoteDesktopFunctionalTest extends WebTestCase
 
         $this->assertContains('My first remote desktop', $crawler->filter('h2')->first()->text());
 
-        $this->assertContains('Gaming: Preinstalled with Steam, UPlay, GOG and Origin', $crawler->filter('div.remotedesktop-infobox')->first()->text());
+        $this->assertContains('Gaming: Preinstalled with Steam®, Uplay™, GOG.com™ and Origin™', $crawler->filter('div.remotedesktop-infobox')->first()->text());
 
         $this->assertContains('Current status:', $crawler->filter('h3')->first()->text());
         $this->assertContains('Not running', $crawler->filter('.remotedesktopstatus')->first()->text());
