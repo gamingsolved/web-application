308ed6d14cbd3768620368324dff64fa2d6cb2ba
Manuel Kiessling <manuel@kiessling.net>
2017-06-24 10:32:38 +0200

    Working on administration functionality.

---
+++ tests/Functional/AdministrationFunctionalTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\Helpers\Helpers;
+
+class LogoutFunctionalTest extends WebTestCase
+{
+    use Helpers;
+
+
+    public function testNonAdminUserCannotReachAdministrationArea()
+    {
+        $this->resetDatabase();
+
+        $client = $this->getClientThatRegisteredAndActivatedAUser();
+        $client->request('GET', '/en/administration/');
+
+        $this->assertEquals(403, $client->getResponse()->getStatusCode());
+    }
+
+}
